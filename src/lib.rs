1282feca55f2ea3b82a50b54440fc1bedf6ab42d
gnzlbg <gonzalobg88@gmail.com>
2018-06-26 12:16:54 +0200

    fix doc warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
-//! A macro for defining #[cfg] if-else statements.
+//! A macro for defining `#[cfg]` if-else statements.
 //!
 //! The macro provided by this crate, `cfg_if`, is similar to the `if/elif` C
 //! preprocessor macro by allowing definition of a cascade of `#[cfg]` cases,
 //! emitting the implementation which matches first.
 //!
-//! This allows you to conveniently provide a long list #[cfg]'d blocks of code
+//! This allows you to conveniently provide a long list `#[cfg]`'d blocks of code
 //! without having to rewrite each clause multiple times.
 //!
 //! # Example
